8bcafdc674cc63a0110a082d62686a08a6455d53
caffeine <caffeine.wbridges@gmail.com>
2024-04-24 16:10:31 +0800

    chore: add font family and install clsx

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,7 +1,14 @@
-import { DM_Sans } from "next/font/google";
+import { DM_Sans, DM_Mono } from "next/font/google";
 
 export const dm_sans = DM_Sans({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-dm_sans',
+})
+
+export const dm_mono = DM_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-dm_mono',
+  weight: ['300', '400', '500'],
 })
